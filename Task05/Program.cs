35479965ecd9f21346a14427bcd46f3513bc419f
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-09-25 20:32:39 +0300

    Выполненно Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу,которая на вход принимает одно число (N),
+//а на выходе показывает все целые цисла в промежутке от -N до N.
+// 4-> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2-> "-2, -1, 0, 1, 2"
+
+
+Console.WriteLine("Введите целое положительное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+if (number > 0)
+{
+
+    while (count <= number)
+    {
+        Console.WriteLine($"{count} ");
+        count++;
+    }
+}
+else Console.WriteLine("Некорректный ввод");
