65ca6bb3e5b53ab750ddde27bcfe12bf4a33fe3d
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-09-25 22:20:50 +0300

    Выполнила Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу,
+//1.которая выводит случайное число из отрезка [10,99]
+//2.и показыввает наибольшую цифру числа.
+
+//78 -> 8
+//12 -> 2
+//85 -> 8
+
+int number = new Random().Next(10, 100); // max = 99 + 1
+Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
+
+//int firstDigit = number / 10;   // 78 / 10 = 7
+//int secondDigit = number % 10;
+
+//Условный оператор 
+//if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+//else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+//Тернарный оператор
+//int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+//Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+
+
+//Реализация кода с помощью функции(метода)
+
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
