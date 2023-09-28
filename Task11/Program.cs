1435196deb1bef44612dd0e7af91a7d02d13f572
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-09-28 15:02:34 +0300

    Выполнен Task11 и использован метод

---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напиши программу,которая выводит случайное трёхзначное чило 
+и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+/*int number = new Random().Next(100, 1000);
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int numResult = firstDigit * 10 + lastDigit;
+Console.WriteLine($"{number} -> {numResult}");*/
+
+//Метод
+
+int number = new Random().Next(100, 1000);
+
+int deleteSecondDigit = DeleteSecondDigit(number);
+Console.WriteLine($"{number} -> {deleteSecondDigit}");
+
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int numResult = firstDigit * 10 + lastDigit;
+    return numResult;
+}
