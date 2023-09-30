9bfd1304c3ecc849dba69ffe582df958cd03c21c
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-09-30 16:12:51 +0300

    Выполнен Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу,которая принимает на вход
+два числа и проверяет,является ли одно число
+квадратом второго.
+5, 25 -> да
+-4,16 -> да
+25,5  -> да
+8, 9  -> нет */
+
+bool IsOneSquareOfTheOther (int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+Console.WriteLine($"Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsOneSquareOfTheOther(number1, number2);
+Console.WriteLine (result ? "да" : "нет");
