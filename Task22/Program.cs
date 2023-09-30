ac890a4df30cd82426624c7c031a062701aead96
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-09-30 15:51:49 +0300

    Выполнен Task22 в этом упражнение выводили таблицу и метод void использовали

---
+++ Task22/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу,которая принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+
+*/
+
+
+void TableSquare(int num)      /*void - это ключевое слово которое говорит что мы не чего
+                                        не возвращаем. Это слово испольщуется в методе.*/ 
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} {count*count}");
+        /*Console.WriteLine($"{count,3} {count*count,5}");  
+        Это формтирование кода, для терминала*/
+        count++;
+    }
+}
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
