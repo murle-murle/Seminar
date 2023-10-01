211974102abeb635872cc0e524932113e60adb75
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-01 17:29:57 +0300

    Выполнен Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу,которая принимает на вход число N
+и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+
+int Factorial(int num)
+{
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            res = res * i;
+        }
+
+    }
+    return res;
+}
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение числа от 1 до {number} -> {factorial}");
