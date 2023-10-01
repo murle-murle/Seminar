8c541b485ea5fb8ea472266799ec2625fe814efb
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-01 17:04:19 +0300

    Выполнен Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу,которая принимает на вход число
+и выдаёт количество цифр в числе
+456 ->3
+78  ->2
+89126 ->5
+*/
+
+int CountDigit(int num)
+{
+    if (num == 0) return 1;
+    if (num < 0) num = num * -1;
+    int count = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = CountDigit(number);
+Console.WriteLine($"Колличество цифр в числе {result}");
