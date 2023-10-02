e954030c8ad6986630b5fddb47b18cac112320ed
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-02 13:24:59 +0300

    Выполнен Task23, с ошибкой,хотя у меня решение проще

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -32,6 +32,8 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 ShowCube(number);*/
 
+
+/*
 public class Answer
 {
     static void ShowCube(int N)
@@ -56,6 +58,41 @@ public class Answer
         {
             // Здесь вы можете поменять значения для отправки кода на Выполнение
             N = 3;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}*/
+
+using System;
+
+public class Answer
+{
+   static void ShowCube(int N)
+    {
+      for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(Cube (i));
+        }// Введите свое решение ниже
+        
+    }
+public static int Cube(int number)
+{
+  return number * number * number;
+}
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) 
+     {
+        int N;
+
+        if (args.Length >= 1) 
+        {
+            N = int.Parse(args[0]);
+        } else
+        {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 5;
         }
 
         // Не удаляйте строки ниже
