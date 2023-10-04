d94dc21ff76ab0a8912846bea9f844837eed90e9
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-04 22:33:56 +0300

    Выполнен Task33. массивы и случ числа

---
+++ Task33/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задайте массив.Напишите программу,которая определяет,
+присутсвует ли заданное число в массиве.
+ 4; массив [6,7,19,345,3] -> нет
+ 3; массив [6,7,19,345,3] -> да
+*/
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+bool FindNumber(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number) return true;
+    }
+    return false;
+}
+
+int[] array = CreateArrayRndInt(5, -10, 10);
+PrintArray(array);
+Console.WriteLine("Введите число для поиска");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = FindNumber(array, num);
+Console.WriteLine(result ? "Искомое число найдено" : "Искомое число не найдено");
