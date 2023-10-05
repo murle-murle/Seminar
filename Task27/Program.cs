4667f89f82fa8f3c2167b16397344dc671b56d7e
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-05 09:40:28 +0300

    Выполнен Task27.метод есть

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumOfDigits(int num)
+{
+    int result = 0;
+
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumOfDigits(number);
+Console.WriteLine($"Сумма всех чисел в цифре {result} ");
