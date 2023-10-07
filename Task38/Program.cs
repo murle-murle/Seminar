128a45309d119858f7a68e6b7c60a3bcc3fba10b
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-07 17:06:20 +0300

    Выполнен Task38.автотесты

---
+++ Task38/Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+
+public class Answer
+{
+    public static double FindMax(double[] array)
+    {
+        double max = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (max < array[i])
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static double FindMin(double[] array)
+    {
+        double min = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (min > array[i])
+            {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static double CalcDifferenceBetweenMaxMin(double[] array)
+    {
+        return FindMax(array) - FindMin(array);
+    }
+
+    public static void PrintArray(double[] array)
+    {
+        foreach (double number in array)
+        {
+            Console.Write($"{number:f2}\t");
+        }
+        Console.WriteLine();
+    }
+
+    public static void Main(string[] args)
+    {
+        double[] array;
+        if (args.Length == 0)
+        {
+            array = new double[] { 3.17, 8.94, 2.36, 5.72, 0.85 };
+        }
+        else
+        {
+            // Иначе, парсим аргументы в массив чисел
+            string[] argStrings = args[0].Split(", ");
+            array = new double[argStrings.Length];
+            for (int i = 0; i < argStrings.Length; i++)
+            {
+                if (double.TryParse(argStrings[i], out double number))
+                {
+                    array[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+                    return;
+                }
+            }
+        }
+
+        Console.WriteLine("Массив:");
+        PrintArray(array);
+        double diff = CalcDifferenceBetweenMaxMin(array);
+        Console.WriteLine($"Разность между максимальным и минимальным элементом = {diff:f2}");
+    }
+}
