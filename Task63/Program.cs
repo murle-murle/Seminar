d0725aa1f32a5b34a9807dc01116dc64b9b5028c
Anastasiya <Koshkina.anastasiya.1994@mail.ru>
2023-10-17 22:12:30 +0300

    Выполнен Task63 Рекурсия

---
+++ Task63/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте значение N.
+Напишите программу,которая выведет все натуральные числа
+в промежуточные от 1 до N.
+N = 5-> "1,2,3,4,5"
+N = 6-> "1,2,3,4,5,6"
+*/
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+PrintNaturalNumbers(number);
+
+void PrintNaturalNumbers(int num)
+{
+    if(num == 0) return;
+    //Console.Write($"{num} ");   //5 = -> 5 4 3 2 1
+    PrintNaturalNumbers(num - 1);
+    Console.Write($"{num} "); //5 = ->1 2 3 4 5 нам подходит этот вариант.
+}
